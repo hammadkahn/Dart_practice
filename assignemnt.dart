b5c87c798340781f2ff7566af4b1171c4c3daeb7
hammadkahn <hammad.khan0312@gmail.com>
2022-02-04 23:29:26 +0500

    Final commit

--- assignemnt.dart
+++ assignemnt.dart
@@ -22,6 +22,7 @@ void main() {
     },
     {"taskName": "Do Assignment 1", "isCompleted": true, "submissionDate": 5}
   ];
+  print("----------------Task 1-----------------------");
   // Task 1
   List<Map> Completed = [];
   for (var i in Task) {
@@ -31,10 +32,24 @@ void main() {
     }
   }
   print(Completed);
+  print("---------------Task 2------------------------");
   // Task 2
   List<Map> filteredTask = Task.where((task) =>
       task["submissionDate"] >= 10 && task["submissionDate"] <= 20).toList();
   print(filteredTask);
+  print("----------------Task 3-----------------------");
   // Task 3
-  
+  for (var i = 0; i < Task.length; i++) {
+    var a = (Task[i]["isCompleted"]);
+    if (a == true) {
+      Task[i]["taskName"] = "Completed";
+    }
+  }
+  print(Task);
+  print("----------------Task4-----------------------");
+  //Task 4
+  var list = [Task, Completed, filteredTask];
+  list.forEach((item) {
+    print('${list.indexOf(item)} : $item');
+  });
 }
