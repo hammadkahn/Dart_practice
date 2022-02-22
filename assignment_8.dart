62ecd771c728aeef1bd2f05f19b581bcdf261da5
hammadkahn <hammad.khan0312@gmail.com>
2022-02-22 17:20:43 +0500

    Assignment 8

--- assignment_8.dart
+++ assignment_8.dart
@@ -1,10 +1,19 @@
-import 'dart:ffi';
-
-void main() {}
+void main() {
+  Iphone12 apple = Iphone12("512gb", "Iphone12 pro max", 11, 250000);
+  print(apple.Space);
+}
 
 abstract class Product {
   String name = "";
   int stockQuantity = 0;
   double price = 0;
-  
+  Product(this.name, this.price, this.stockQuantity);
+}
+
+class Iphone12 extends Product {
+  String Space = "";
+  // @override
+  // set name(String _name) {
+  //   super.name = "iphone 12 128gb";
+  Iphone12(this.Space, String n, int s, double p) : super(n, p, s);
 }
